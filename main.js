a5dd566b782ea891b21991b890bb008c46251f39
Azdy Fahmi <azdy.fm@gmail.com>
2025-03-15 16:24:45 +0700

    L4 solution

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
+      inventory: 1000,
+      // Solution
+      onSale: true,
+      // Solution
     };
   },
 });
