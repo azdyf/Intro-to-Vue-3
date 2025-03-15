cb38ba7d3d5384351277b8e76afd9433a8dcddd7
Azdy Fahmi <azdy.fm@gmail.com>
2025-03-15 22:46:30 +0700

    L10 solution

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,16 @@ app.component("product-display", {
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <!-- solution -->
+          <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          @click="removeFromCart">
+          Remove Item
+        </button>
+       <!-- solution -->
       </div>
     </div>
   </div>`,
@@ -59,18 +69,23 @@ app.component("product-display", {
           id: 2235,
           color: "blue",
           image: "./assets/images/socks_blue.jpg",
-          quantity: 0,
+          quantity: 1,
         },
       ],
     };
   },
   methods: {
     addToCart() {
-      this.cart += 1;
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
     },
     updateVariant(index) {
       this.selectedVariant = index;
     },
+    // solution
+    removeFromCart() {
+      this.$emit("remove-from-cart", this.variants[this.selectedVariant].id);
+    },
+    // solution
   },
   computed: {
     title() {
