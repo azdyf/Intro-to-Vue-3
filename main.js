40b508ae18afc92e5b15a84ec9822bba7cb75c8b
Azdy Fahmi <azdy.fm@gmail.com>
2025-03-15 16:12:15 +0700

    L3 solution

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
+      image: "./assets/images/socks_green.jpg",
+      // solution
+      url: "https://www.vuemastery.com/",
+      // solution
     };
   },
 });
